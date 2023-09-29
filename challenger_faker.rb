3800f8b1d64d53562525b4fbd9df5849b10d5231
TallAsian <elijah.rohi@gmail.com>
2023-09-29 17:28:43 -0500

    bla

--- challenger_faker.rb
+++ challenger_faker.rb
@@ -1,18 +1,16 @@
 require_relative 'ar.rb'
 
-x=0
-y=0
 10.times do
     tests = Category.new(
         name: Faker::Food.dish,
         description: Faker::Food.description
     )
-    10.times do
-        testing = Product.new(
-            name: Faker::Food.allergen,
-            description: Faker::Food.description,
-            price: Faker::Number.between(from: 1, to: 1000),
-            stock_quantity: Faker::Number.between(from: 1, to: 1000)
-        )
-    end
+    # 10.times do
+    #     testing = Product.new(
+    #         name: Faker::Food.allergen,
+    #         description: Faker::Food.description,
+    #         price: Faker::Number.between(from: 1, to: 1000),
+    #         stock_quantity: Faker::Number.between(from: 1, to: 1000)
+    #     )
+    # end
 end
