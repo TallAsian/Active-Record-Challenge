17dd3a7d5bf57dfa77247ccb59c4b1efe0a435c6
TallAsian <elijah.rohi@gmail.com>
2023-09-29 17:18:41 -0500

    commit 2

--- challenger_faker.rb
+++ challenger_faker.rb
@@ -1,8 +1,16 @@
 require_relative 'ar.rb'
 
 while x < 10
-    Category.new(
-        name: Faker::Food.dish
+    tests = Category.new(
+        name: Faker::Food.dish,
         description: Faker::Food.description
     )
+    while y < 10
+        testing = Product.new(
+            name: Faker::Food.allergen,
+            description: Faker::Food.description,
+            price: Faker:Number.number(from: 1, to: 4),
+            stock_quantity: Faker:Number.number(from:1, to: 3)
+        )
+    end
 end
